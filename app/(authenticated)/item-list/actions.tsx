7f9339d9e70fa2a61a3c673df617d29799f2f73b
wichaiwon <wichai.wongfu.pkg@gmail.com>
2026-03-06 14:12:22 +0700

    ย้ายการเรียกใช้ api ไปที่ actions.tsx Move API calls to actions.tsx

--- app/(authenticated)/item-list/actions.tsx
+++ app/(authenticated)/item-list/actions.tsx
@@ -1,7 +1,31 @@
 'use server'
 
-import { Customer, getCustomers, PaginatedResponse } from '@/lib/customer'
+import {
+  Customer,
+  getCustomers,
+  getCustomerById,
+  createCustomer,
+  updateCustomer,
+  deleteCustomer,
+  PaginatedResponse,
+} from '@/lib/customer'
 
-export async function fetchCustomers(page: number, pageSize: number): Promise<PaginatedResponse<Customer>> {
-  return getCustomers(page, pageSize)
+export async function fetchCustomers(page: number, pageSize: number,search?:string): Promise<PaginatedResponse<Customer>> {
+  return getCustomers(page, pageSize,search)
+}
+
+export async function fetchCustomerById(id: string): Promise<Customer> {
+  return getCustomerById(id)
+}
+
+export async function addCustomer(firstname: string, lastname: string): Promise<Customer> {
+  return createCustomer(firstname, lastname)
+}
+
+export async function editCustomer(firstname: string, lastname: string, id: string): Promise<Customer> {
+  return updateCustomer(firstname, lastname, id)
+}
+
+export async function removeCustomer(id: string): Promise<void> {
+  return deleteCustomer(id)
 }
